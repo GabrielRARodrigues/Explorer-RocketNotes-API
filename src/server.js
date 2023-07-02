61a558fb96da33cc65a19de969d2bf9ed2ec85c0
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-07-02 20:20:44 -0300

    Continuing the development of the API, working with the GET method and request parameters

--- src/server.js
+++ src/server.js
@@ -2,7 +2,20 @@ import express from 'express'
 
 const app = express()
 
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params
+
+  response.send(
+    `<h1>Mensagem ID: ${id}.
+     Para o usuário: ${user} </h1>`
+  )
+})
+
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`<h1>Página: ${page}. Mostrar: ${limit}</h1>`)
+})
+
 const PORT = 3333
-
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
-
