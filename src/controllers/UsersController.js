c67963f4872ab0830d3824ea43810cf746938caf
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-08-01 09:28:57 -0300

    Implementing the logic to register user and encrypting the user password
    Modifying the create method of the UsersController class, effectively implementing the logic to insert users and installing the bcryptjs library to encrypt the user's password using the hash method

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,5 @@
+import bcryptjs from 'bcryptjs'
+const { hash } = bcryptjs
 import AppError from '../utils/AppError.js'
 import sqliteConnection from '../database/sqlite/index.cjs'
 
@@ -14,11 +16,21 @@ class UsersController {
     const { name, email, password } = request.body
 
     const database = await sqliteConnection()
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const checkUserExists = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
 
-    if(checkUserExists){
-      throw new AppError("Este email já está em uso.")
+    if (checkUserExists) {
+      throw new AppError('Este email já está em uso.')
     }
+
+    const hashedPassword = await hash(password, 8)
+
+    await database.run(
+      'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
+    )
 
     return response.status(201).json()
   }
