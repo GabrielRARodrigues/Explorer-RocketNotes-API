1694ae8292e9f122fd6a53ad025aaf2a2a7c1c5c
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-08-06 21:38:38 -0300

    Adding the functionality to modify user information in the database
    Adding the functionality to modify the data of a registered user in the database, we will create a new route using the HTTP PUT method, providing the user's ID that will have the updated data. We will implement the logic to update the data by creating the "update" method in the "UsersController" class to perform data checks and update the database with the new information provided in the request body

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
 import bcryptjs from 'bcryptjs'
-const { hash } = bcryptjs
+const { hash, compare } = bcryptjs
 import AppError from '../utils/AppError.js'
 import sqliteConnection from '../database/sqlite/index.cjs'
 
@@ -33,6 +33,59 @@ class UsersController {
     )
 
     return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado')
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('Este e-mail já está em uso.')
+    }
+
+    user.name = name
+    user.email = email
+
+    if (password && !old_password) {
+      throw new AppError(
+        'Você precisa informar a senha antiga para definir a nova senha'
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha antiga não confere')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(
+      ` 
+    UPDATE users SET
+    name = ?,
+    email = ?,
+    password = ?,
+    updated_at = ?
+    WHERE id = ?`,
+      [user.name, user.email, user.password, new Date(), id]
+    )
+
+    return response.status(200).json()
   }
 }
 
