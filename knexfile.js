5aa6d35d313a1519ce7174956eee0f4273522f64
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-08-13 20:42:42 -0300

    Using Knex.js to create the 'notes' table and its migration
    Modifying the knenfile.js file to use CommonJS instead of ES Modules, and configuring the Knex library for creating migrations, then creating the migration for the Notes table and registering this table in the database

--- knexfile.js
+++
@@ -1,9 +0,0 @@
-import path from 'path'
-
-export const development = {
-  client: 'sqlite3',
-  connection: {
-    filename: path.resolve(__dirname, 'src', 'database', 'database.db')
-  },
-  useNullAsDefault: true
-}
