e50d7d75cac701cbbdfab72d629924811cc1c8d7
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-08-17 14:54:02 -0300

    Creating migrations for the tags and links tables and modifying knexfile.js
    Creating the migrations to assemble the 'links' and 'tags' tables with their respective fields, and establishing the relationship between the tables through foreign keys

---
+++ src/database/knex/migrations/20230817004137_createTags.js
@@ -0,0 +1,15 @@
+export const up = knex =>
+  knex.schema.createTable('tags', table => {
+    table.increments('id')
+    table.text('name').notNullable()
+
+    table
+      .integer('note_id')
+      .references('id')
+      .inTable('notes')
+      .onDelete('CASCADE')
+
+    table.integer('user_id').references('id').inTable('user')
+  })
+
+export const down = knex => knex.schema.dropTable('tags')
