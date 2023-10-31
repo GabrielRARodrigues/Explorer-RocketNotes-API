abab2a3dc2a2e4c047bd8a05bd3b13783b07bf6c
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-10-30 21:22:58 -0300

    Completing the authentication logic by creating the 'ensureAuthenticated' middleware
    Concluding the development of the code responsible for user authentication, creating the 'middlewares' folder and the 'ensureAuthenticated.js' middleware that will intercept requests and verify if the authentication token has been provided, throwing an error if authentication is not satisfied. If authentication is successful and there is a token present, the controller responsible for the request will be called, receiving the user's ID in the request body. Modifying the routes within the 'routes' folder to implement the 'ensureAuthenticated' middleware. Finally, updating the application's controllers to receive the user's ID in the request body instead of as a parameter

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 
 import NotesController from '../controllers/NotesController.js'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated.js'
 
 const notesRoutes = Router()
 
 const notesController = new NotesController()
 
+notesRoutes.use(ensureAuthenticated)
+
+notesRoutes.post('/', notesController.create)
 notesRoutes.get('/', notesController.index)
-notesRoutes.post('/:user_id', notesController.create)
 notesRoutes.get('/:id', notesController.show)
 notesRoutes.delete('/:id', notesController.delete)
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import UsersController from '../controllers/UsersController.js'
+import ensureAuthenticated from '../middlewares/ensureAuthenticated.js'
 
 const userRoutes = Router()
 
 const usersController = new UsersController()
 
 userRoutes.post('/', usersController.create)
-userRoutes.put('/:id', usersController.update)
+userRoutes.put('/', ensureAuthenticated, usersController.update)
 
 export default userRoutes
